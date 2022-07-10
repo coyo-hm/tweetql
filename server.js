b0d607b5e8a54914b08bcdc7371acd88b565fbf9
DADA YO <dayeon.yoon@gmail.com>
2022-07-10 19:45:53 +0900

    FEAT: Resolver 추가

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
 import {ApolloServer, gql} from "apollo-server";
 
-const tweets = [
+let tweets = [
     {
         id: "1",
         text: "first",
+        userId: "2",
     },
     {
         id: "2",
         text: "second",
+        userId: "1",
     },
     {
         id: "3",
         text: "third",
+        userId: "2",
     }
 ]
+
+let users = [
+    {
+        id: "1",
+        firstName: "first",
+        lastName: "last",
+
+    },
+    {
+        id: "2",
+        firstName: "secondUser",
+        lastName: "secondUserLast",
+
+    },
+];
 
 const typeDefs = gql`
     type User{
         id: ID!
-        username: String!
         firstName: String!
-        lastName: String
+        lastName: String!
+        fullName: String!
     }
+    """
+    Tweet desctiption
+    """
     type Tweet{
         id: ID!
         text: String!
         author: User
     }
     type Query{
+        allUsers: [User]!
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
     }
@@ -44,6 +66,35 @@ const resolvers = {
         },
         tweet(root, {id}){
             return tweets.find((tweet) => tweet.id === id);
+        },
+        allUsers(){
+            return users;
+        }
+    }, 
+    Mutation:{
+        postTweet(root, {text, userId}){
+            const newTweet = {
+                id: tweets.length + 1,
+                text
+            };
+            tweets.push(newTweet)
+            return newTweet
+        },
+        deleteTweet(root, {id}){
+            const tweet = tweets.find((tweet) => tweet.id === id);
+            if (!tweet) return false;
+            tweets = tweets.filter((tweet) => tweet.id !== id);
+            return true;
+        }
+    },
+    User: {
+        fullName({firstName, lastName}){
+            return `${firstName} ${lastName}`;
+        }
+    },
+    Tweet: {
+        author({userId}){
+            return users.find(user => user.id === userId)
         }
     }
 }
@@ -51,4 +102,4 @@ const resolvers = {
 const server = new ApolloServer({typeDefs, resolvers})
 server.listen().then(({ url })=>{
     console.log(`Running on ${url}`)
-});
+}); 
